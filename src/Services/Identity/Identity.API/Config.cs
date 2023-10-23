eb6676280c91c5187ce64e04d69019b5d0b9f4df
James Tran Van Quyen <quyen.tranvan.orc@ascentis.com.sg>
2023-10-24 05:33:00 +0700

    Added WebMVC project

--- src/Services/Identity/Identity.API/Config.cs
+++ src/Services/Identity/Identity.API/Config.cs
@@ -5,7 +5,7 @@
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
-namespace Identity.API
+namespace OrionEShopOnContainer.Services.Identity.API
 {
     public static class Config
     {
--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Identity.API
+namespace OrionEShopOnContainer.Services.Identity.API
 {
     public class Startup
     {

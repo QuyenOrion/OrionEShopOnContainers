cdd1ad9c6ee254ed1b6f2fab1f788e7d6374ae01
James Tran Van Quyen <quyen.tranvan.orc@ascentis.com.sg>
2024-03-09 21:59:49 +0700

    Updated catalog-api chart

--- src/Services/Catalog/Catalog.API/Extensions/Extensions.cs
+++ src/Services/Catalog/Catalog.API/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using OrionEShopOnContainers.Services.Catalog.API.Infrastructure;
 
 namespace OrionEShopOnContainers.Services.Catalog.API.Extensions;
@@ -11,6 +12,17 @@ public static class Extensions
         {
             sqlOptions.MigrationsAssembly(typeof(Program).Assembly.GetName().Name);
             sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorCodesToAdd: null);
+        }
+
+        string connectionString = configuration.GetConnectionString("Default");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            var postgresHost = configuration.GetConnectionString("PostgresHost");
+            var database = configuration.GetConnectionString("PostgresDb");
+            var username = configuration.GetConnectionString("PostgresUser");
+            var password = configuration.GetConnectionString("PostgresPassword");
+            var options = configuration.GetConnectionString("PostgresOptions");
+            connectionString = $"Host={postgresHost};Database={database};Username={username};Password={password};{options}";
         }
 
         services.AddDbContext<CatalogContext>(options =>

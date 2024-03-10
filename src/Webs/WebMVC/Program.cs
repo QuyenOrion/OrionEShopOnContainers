f7dbfd74bf39b621b950e0a1baa568f67d802390
James Tran Van Quyen <quyen.tranvan.orc@ascentis.com.sg>
2024-03-11 03:56:48 +0700

    Modified webmvc chart and added healthcheck to webmvc

--- src/Webs/WebMVC/Program.cs
+++ src/Webs/WebMVC/Program.cs
@@ -1,4 +1,8 @@
+using OrionEShopOnContainers.Services.Service.Common;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -16,6 +20,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseServiceDefault();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
